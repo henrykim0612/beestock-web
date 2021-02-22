ec20e960f940e588a4387edab128eeec26f0b19c
herny.kim <hyeongjong90@gmail.com>
2021-02-22 22:44:53 +0900

    국내 포트 나오지 않는 문제 수정

--- src/main/webapp/js/analysis/profile_analysis.js
+++ src/main/webapp/js/analysis/profile_analysis.js
@@ -715,7 +715,7 @@ const main = (function() {
     }]);
     cmmUtils.setTippy([{
       selector: '.currPriceHeader',
-      content: '장 중 10분마다 최신 현재주가를 반영하여<br/>비중, 수익률이 재계산됩니다.<br/>※ 국내는 서비스 준비중 입니다.',
+      content: '장 중 1분마다 최신 현재주가를 반영하여<br/>비중, 수익률이 재계산됩니다.<br/>※ 국내는 서비스 준비중 입니다.',
       allowHTML: true
     }]);
     cmmUtils.setTippy([{selector: '.bpHeader', content: '단위: 달러'}]);
