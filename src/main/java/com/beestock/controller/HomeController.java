3da4a731f770a988ac9ae332556136026d4842bc
herny.kim <hyeongjong90@gmail.com>
2021-02-24 00:35:11 +0900

    # 수정사항   - properties 공통화

--- src/main/java/com/beestock/controller/HomeController.java
+++ src/main/java/com/beestock/controller/HomeController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@SessionAttributes("loginSession")
 @RequestMapping("/home")
 public class HomeController {
 
